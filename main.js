f11bc37db1cd0c06ce01d6bca0093167e775a9f3
Kamil Chodowski <kchodowski@gmail.com>
2020-08-02 17:17:59 +0100

    adding hamburger menu

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 const name = "Kamil";
 const age = 33;
 
-console.log(
-  `Cześć! Mam na imie ${name} i mam ${age} lata.
-A to jest moja strona z notatkami ze szkolenia WTF:Co ten frontend`
-);
+const emptySelector = document.querySelector(".about__description--js");
+
+emptySelector.innerHTML = "Ta treść została dodana w JavaScript";
+
+function greet(age, name) {
+  console.log(`Cześć! Mam na imie ${name} i mam ${age} lata.`);
+}
+
+greet(age, name);
+
+const greetNew = (age, name) => {
+  console.log(`Cześć! Jestem ${name} i mam ${age} lata!`);
+};
+
+greetNew(33, "Kamil");
+
+if (3 === 3) {
+  console.log("To prawda");
+}
+
+const hamburger = document.querySelector(".hamburger--js");
+
+hamburger.addEventListener("click", () => {
+  const nav = document.querySelector(".navigation--js");
+  nav.classList.toggle("navigation--open");
+});
